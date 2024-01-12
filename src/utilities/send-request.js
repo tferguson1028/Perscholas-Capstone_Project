dd6be6606158bee0504487e07f14961418b14924
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-12 13:57:14 -0500

    Implementing api code

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,4 +1,3 @@
-
 import { getToken } from './users-service';
 export async function sendRequest(url, method = 'GET', payload = null) {
     // Fetch accepts an options object as the 2nd argument
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,35 +1,41 @@
-import * as usersAPI from "./users-api"; 
+import * as usersAPI from "./users-api";
 
 
-export function checkToken(){
-  return usersAPI.checkToken()
-    // checkToken returns a string, but let's
-    // make it a Date object for more flexibility
-    .then(dateStr => new Date(dateStr));
+export function checkToken()
+{
+  // checkToken returns a string, but let's 
+  // make it a Date object for more flexibility
+  return usersAPI.checkToken().then(dateStr => new Date(dateStr));
 }
 
-export async function login(credentials){
-    const token = await usersAPI.login(credentials)
-    console.log(`Token: ${token}`)
-    localStorage.setItem('token', token)
-    console.log("Check Local Storage")
-    return getUser()
-  }
+export async function login(credentials)
+{
+  const token = await usersAPI.login(credentials);
+  console.log(`Token: ${token}`);
+  
+  localStorage.setItem('token', token);
+  console.log("Check Local Storage");
+  
+  return getUser();
+}
 
-  export async function signUp(userData) {
-    const token = await usersAPI.signUp(userData);
-    localStorage.setItem("token", token);
-    return token;
-  }
+export async function signUp(userData)
+{
+  const token = await usersAPI.signUp(userData);
+  localStorage.setItem("token", token);
+  return token;
+}
 
-export function getToken() {
+export function getToken()
+{
   // getItem returns null if there's no string
   const token = localStorage.getItem("token");
   if (!token) return null;
   // Obtain the payload of the token
   const payload = JSON.parse(atob(token.split(".")[1]));
   // A JWT's exp is expressed in seconds, not milliseconds, so convert
-  if (payload.exp < Date.now() / 1000) {
+  if (payload.exp < Date.now() / 1000)
+  {
     // Token has expired - remove it from localStorage
     localStorage.removeItem("token");
     return null;
@@ -37,12 +43,14 @@ export function getToken() {
   return token;
 }
 
-export function getUser() {
+export function getUser()
+{
   const token = getToken();
   // If there's a token, return the user in the payload, otherwise return null
   return token ? JSON.parse(atob(token.split(".")[1])).user : null;
 }
 
-export function logOut() {
+export function logOut()
+{
   localStorage.removeItem('token');
-  }
+}
