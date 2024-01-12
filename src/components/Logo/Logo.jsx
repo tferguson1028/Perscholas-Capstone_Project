9260da3f4a1252089ac2099415d66f3ca3736dc3
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-12 15:21:01 -0500

    Implementing components and styling

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import styles from "../../styles/Logo.module.css"
 
 function Logo()
 {
   return (
-    <div>Logo</div>
-  )
+    <div className={styles.Logo}>
+      <div>SEI</div>
+      <div>CAFE</div>
+    </div>
+  );
 }
 
 export default Logo;
