eb2689c209aa9cbed2d6b9e00431e1b14cae051e
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-12 14:27:12 -0500

    Added style file and moved logout functionally to individual component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import * as userService from '../utilities/users-service'
-function Navbar({user ,setUser}) {
+import {Link} from "react-router-dom";
+import UserLogOut from "./UserLogOut/UserLogOut";
 
-  function handleLogOut() {
-    // Delegate to the users-service
-    userService.logOut();
-    // Update state will also cause a re-render
-    setUser(null);
-  }
+function Navbar(props)
+{
+  const { user, setUser } = props;
+  
   return (
     <>
-      <nav style={{ justifyContent: "space-evenly", display: "flex" }}>
+      <nav style={{justifyContent: "space-evenly", display: "flex"}}>
 
-      <div style={{justifyContent:'space-around'}}>
-        <p style={{margin:'1em'}}>Welcome {user.name}, </p><hr/>
-        <p style={{margin:'1em'}}> Logged In : {user.email}</p>
-        <Link to="" onClick={handleLogOut}><button>Logout</button></Link>
-      </div>
+        <div style={{justifyContent: 'space-around'}}>
+          <p style={{margin: '1em'}}>Welcome {user.name}, </p><hr />
+          <p style={{margin: '1em'}}> Logged In : {user.email}</p>
+          <UserLogOut setUser={setUser} />
+        </div>
 
         <Link to="/orders">Order History</Link>
 
--- src/components/UserLogOut/UserLogOut.jsx
+++ src/components/UserLogOut/UserLogOut.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+import * as userService from '../../utilities/users-service';
 
 function UserLogOut()
 {
+  function handleLogOut(props) 
+  {
+    const { setUser = () => {} } = props;
+    // Delegate to the users-service
+    // Update state will also cause a re-render
+    userService.logOut();
+    setUser(null);
+  }
+  
   return (
-    <div>UserLogOut</div>
+    <>
+      <Link to="" onClick={handleLogOut}><button>Logout</button></Link>
+    </>
   )
 }
 
