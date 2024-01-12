af092bcfdfa53adb3821548b96ba478fae3236bd
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-12 12:06:45 -0500

    Styling and page functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import {useState} from "react";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import {getUser} from "./utilities/users-service";
 import NewOrderPage from "./pages/NewOrderPage/NewOrderPage.jsx";
 import AuthPage from "./pages/AuthPage/AuthPage.jsx";
@@ -12,6 +12,7 @@ import Navbar from "./components/Navbar";
 function App()
 {
   const [user, setUser] = useState(getUser());
+  
   return (
     <div className="App">
 
@@ -19,8 +20,9 @@ function App()
         <>
           <Navbar user={user} setUser={setUser} />
           <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/orders/new" element={<NewOrderPage setUser={setUser} />} />
+            <Route path="/orders" element={<OrderHistoryPage setUser={setUser} />} />
+            <Route path="/*" element={<Navigate to="/orders/new" />} /> {/* Reroute */}
           </Routes>
         </>
       ) : (
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import styles from "../../styles/AuthPage.css";
 
-function AuthPage() {
-  return<>
-    <SignUpForm />
-    <LoginForm />
-  </>;
+function AuthPage(props)
+{
+  const { setUser } = props;
+  const [ showLogin, setShowLogin ] = useState(true);
+  
+  return (
+    <main className={styles.AuthPage}>
+      <div>
+        <span></span>
+        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+      </div>
+      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+    </main>
+  );
 }
 
 export default AuthPage;
