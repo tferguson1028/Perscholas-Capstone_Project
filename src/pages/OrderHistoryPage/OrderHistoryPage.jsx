e60c93a78ff94c0cef20c83d16e4aa4bc5a4013e
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-12 14:44:25 -0500

    Added code for order history page

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,21 +1,62 @@
-import React from 'react'
-import * as usersService from "../../utilities/users-service"; 
+import React, {useEffect, useState} from 'react'
+import {Link} from 'react-router-dom';
+import * as usersService from "../../utilities/users-service";
+import * as ordersAPI from '../../utilities/orders-api';
 
-function OrderHistoryPage() {
+import Logo from '../../components/Logo/Logo';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+import OrdersList from '../../components/OrdersList/OrdersList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
 
-const handleCheckToken = () =>{
-  let exp = usersService.checkToken()
-  console.log(exp)
-}
+import styles from "../../styles/OrderHistoryPage.module.css";
 
+function OrderHistoryPage(props)
+{
+  const { user = {}, setUser = () => {} } = props;
+  const [ orders, setOrders ] = useState([]);
+  const [ activeOrder, setActiveOrder ] = useState(null);
+  
+  useEffect(() => { fetchOrderHistory(); }, []);
+  
+  async function fetchOrderHistory() 
+  {
+    const orders = await ordersAPI.getOrderHistory();
+    setOrders(orders);
+    setActiveOrder(orders[0] || null);
+  }
+  
+  //# Event Handlers
+  async function handleCheckToken()
+  {
+    let exp = await usersService.checkToken();
+    alert(exp);
+  }
+  
+  function handleSelectOrder(order) { setActiveOrder(order); }
 
+  // return (
+  //   <>
+  //     <h1> OrderHistory Page</h1>
+  //     <button onClick={handleCheckToken}>Check Login Expiration</button>
+  //   </>
+  // );
+  
   return (
-    <>
-    <h1> OrderHistory Page</h1>
-    <button onClick={handleCheckToken}> Check Login Expiration </button>
-    
-    </>
-  )
+    <main className={styles.OrderHistoryPage}>
+      <aside className={styles.aside}>
+        <Logo />
+        <Link to="/orders/new" className="button btn-sm">NEW ORDER</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <OrdersList
+        orders={orders}
+        activeOrder={activeOrder}
+        handleSelectOrder={handleSelectOrder}
+      />
+      
+      <OrderDetail order={activeOrder} />
+    </main>
+  );
 }
 
 export default OrderHistoryPage
