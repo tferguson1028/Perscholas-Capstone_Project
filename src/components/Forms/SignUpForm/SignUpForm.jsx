9ee52cf260d1078d420cb4b15e76dd986185be43
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-16 12:07:56 -0500

    Updated files and folder structure and refactoring

--- src/components/Forms/SignUpForm/SignUpForm.jsx
+++ src/components/Forms/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,8 @@
-import React, {Component} from "react";
-import { signUp } from '../../utilities/users-service';
+import React, { Component } from "react";
 
+import { signUp } from '../../../utilities/users-service';
+
+import styles from "../Form.module.css";
 
 export default class SignUpForm extends Component
 {
@@ -12,33 +14,34 @@ export default class SignUpForm extends Component
     error: "",
   };
 
-  handleChange = (evt) =>
+  handleChange = (event) =>
   {
     this.setState({
-      [evt.target.name]: evt.target.value,
+      [ event.target.name ]: event.target.value,
       error: ''
     });
   };
-  handleSubmit = async (evt) =>
+
+  handleSubmit = async (event) =>
   {
-    evt.preventDefault();
+    event.preventDefault();
     // alert(JSON.stringify(this.state));
     try
     {
-      const formData = {...this.state};
-      
+      const formData = { ...this.state };
+
       delete formData.error;
       delete formData.confirm;
-      
+
       const user = await signUp(formData);
       console.log(user);
       this.props.setUser(user);
-      
+
     } catch (error)
     {
       this.setState({
         error: "Sign Up Failed - Try Again"
-      })
+      });
     }
   };
 
@@ -47,11 +50,14 @@ export default class SignUpForm extends Component
     const disable = this.state.password !== this.state.confirm;
 
     return (
-      <>
-        <div>SignUpForm</div>
+      <div className={styles.Form}>
+        <header className="form-header">
+          <h1>Sign Up</h1>
+        </header>
         <div className="form-container">
-          <form autoComplete="off" onSubmit={this.handleSubmit} style={{margin: "1em"}}>
-            <label>Name</label>
+          {this.state.error.length > 0 ? <p className="error-message">{this.state.error}</p> : <></>}
+          <form autoComplete="off" onSubmit={this.handleSubmit} style={{ margin: "1em" }}>
+            <label htmlFor="name">Username</label>
             <input
               type="text"
               name="name"
@@ -59,7 +65,7 @@ export default class SignUpForm extends Component
               onChange={this.handleChange}
               required
             />
-            <label>Email</label>
+            <label htmlFor="email">Email</label>
             <input
               type="email"
               name="email"
@@ -67,7 +73,7 @@ export default class SignUpForm extends Component
               onChange={this.handleChange}
               required
             />
-            <label>Password</label>
+            <label htmlFor="password">Password</label>
             <input
               type="password"
               name="password"
@@ -75,7 +81,7 @@ export default class SignUpForm extends Component
               onChange={this.handleChange}
               required
             />
-            <label>Confirm</label>
+            <label htmlFor="confirm">Confirm Password</label>
             <input
               type="password"
               name="confirm"
@@ -84,13 +90,11 @@ export default class SignUpForm extends Component
               required
             />
             <button type="submit" disabled={disable}>
-              SIGN UP
+              Sign Up
             </button>
           </form>
         </div>
-        <p className="error-message">{this.state.error}</p>
-
-      </>
+      </div>
     );
   }
 }
